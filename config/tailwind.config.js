6ba803fb70cfc53955fcecc609d771ec46e0d28a
BB-Simon <simon.chowdery@gmail.com>
2023-07-14 08:22:42 +0300

    Add splashs controller, route and views

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'main-blue': '#3778c2',
+        'light-green': '#5fb523',
+        'dark-1': '#434b54',
+        'dark-2': '#545C67',
+        'dark-3': '#86898E',
+        'dark-4': '#989DA3',
+      },
     },
   },
   plugins: [
