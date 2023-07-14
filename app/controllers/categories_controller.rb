b812cb7719520f84f4f208cea45b661ee6ffb1d9
BB-Simon <simon.chowdery@gmail.com>
2023-07-14 10:00:52 +0300

    Add category controller methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,29 @@
 class CategoriesController < ApplicationController
-
   def index
-    @categories = Category.all
+    @categories = current_user.categories
+  end
+
+  def show
+    @category = current_user.categories.find(params[:id])
+    @expenses = @category.expenses.order(created_at: :desc)
   end
 
   def new
+    @category = category.new
   end
 
   def create
+    @category = current_user.categories.new(category_params)
+    if @category.save
+      redirect_to root_path, notice: 'Category created succefully'
+    else
+      render :new, alert: @category.errors.full_messages[0]
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
